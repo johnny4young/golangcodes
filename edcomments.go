585d604365a58ffb96b7d15a2f7bb5b1a2890f5a
johnny4young <asesordeprogramacion@gmail.com>
2020-03-14 21:30:34 -0500

    added routing login an user

--- edcomments.go
+++ edcomments.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+
+	"github.com/urfave/negroni"
+
+	"github.com/golangcodes/routes"
 
 	"github.com/golangcodes/migration"
 )
@@ -16,4 +21,20 @@ func main() {
 		migration.Migrate()
 		log.Println("Finalizó la migración")
 	}
+	// Inicia las rutas
+	router := routes.InitRoutes()
+
+	// Inicia los middlewares
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	// inicia server
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: n,
+	}
+
+	log.Println("Iniciando el servidor en http://localhost:8080")
+	server.ListenAndServe()
+	log.Println("Final del programa")
 }
