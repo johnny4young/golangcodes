46ae89a64ba85bf2d8774d47211c9362e00ca9fe
johnny4young <asesordeprogramacion@gmail.com>
2020-03-15 22:25:58 -0500

    added validate token added create comment

---
+++ controllers/comment.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/golangcodes/commons"
+	"github.com/golangcodes/configuration"
+
+	"github.com/golangcodes/models"
+)
+
+// CommentCreate crea un comentario
+func CommentCreate(w http.ResponseWriter, r *http.Request) {
+	comment := models.Comment{}
+	m := models.Message{}
+
+	err := json.NewDecoder(r.Body).Decode(comment)
+	if err != nil {
+		m.Code = http.StatusBadRequest
+		m.Message = fmt.Sprintf("Error al leer el comentario: %s", err)
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	err = db.Create(&comment).Error
+	if err != nil {
+		m.Code = http.StatusBadRequest
+		m.Message = fmt.Sprintf("Error al registrar el comentario: %s", err)
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	m.Code = http.StatusCreated
+	m.Message = "Comentario creado con éxito"
+	commons.DisplayMessage(w, m)
+}
